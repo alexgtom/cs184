8f7cad1843037a93588780e8f9501676207c1b0b
Alex Tom <alexgtom@berkeley.edu>
2013-09-13 02:10:30 -0700

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 echo "--- Pulling from repo"
+cd ~/cs184
 git pull
+cd ~
 echo "--- Copying files to public_html"
 rm -rf ~/public_html
-cp -R cs184/site/_site ~/public_html
+cp -R ~/cs184/site/_site ~/public_html
 
-echo "--- Changing public_html permissions" 
+echo "--- Changing public_html permissions"
 chmod -R 0755 ~/public_html
 
 echo "--- Done"
