4c0d79142b78f16994866fe1b6ec3cd69dd3d6ca
Alex Tom <alexgtom@berkeley.edu>
2013-10-06 00:04:59 -0700

    Changed x and y in Sample to ints.

--- as2/base/Sample.h
+++ as2/base/Sample.h
@@ -1,4 +1,21 @@
 #ifndef SAMPLE_H_
 #define SAMPLE_H_
 
+class Sample {
+  public:
+    int x, y;
+
+    Sample() {
+    }
+
+    Sample(int x, int y) {
+      this->x = x;
+      this->y = y;
+    }
+
+	bool operator==(const Sample& s) const {
+		return x==s.x && y==s.y;
+	}
+};
+
 #endif
