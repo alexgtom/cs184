4f01237c3e9d0ec6dc3ab7a3ffcae2fa3fde73b0
Alex Tom <alexgtom@berkeley.edu>
2013-10-06 00:39:07 -0700

    Added RayTracer skeleton

--- as2/base/RayTracer.h
+++ as2/base/RayTracer.h
@@ -1,4 +1,15 @@
 #ifndef RAYTRACER_H_
 #define RAYTRACER_H_
 
+#include "Ray.h"
+
+class RayTracer {
+  public:
+    void trace(Ray& ray, Color* color) {
+    }
+
+    void trace(Ray& ray, int depth, Color* color) {
+    }
+};
+
 #endif
