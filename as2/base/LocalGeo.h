099a60f0b19fcf9a5ca3f5106b8e3b0d412c4b2f
Alex Tom <alexgtom@berkeley.edu>
2013-10-06 20:16:19 -0700

    Implemented LocalGeo

--- as2/base/LocalGeo.h
+++ as2/base/LocalGeo.h
@@ -1,4 +1,18 @@
 #ifndef LOCALGEO_H_
 #define LOCALGEO
 
+#include "Point.h"
+
+class LocalGeo {
+  public:
+    Point pos;
+    Normal normal;
+
+    LocalGeo() {}
+    LocalGeo(Point pos, Normal normal) {
+      this->pos = pos;
+      this->normal = normal;
+    }
+};
+
 #endif
