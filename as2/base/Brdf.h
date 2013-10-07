482085304c2bbff88d888a2d7411d4f1841894cc
Alex Tom <alexgtom@berkeley.edu>
2013-10-06 20:14:11 -0700

    Implemented BRDF

--- as2/base/Brdf.h
+++ as2/base/Brdf.h
@@ -1,5 +1,19 @@
 #ifndef BRDF_H_
 #define BRDF_H_
 
+#include "Color.h"
+
+class BRDF {
+  public:
+    Color kd, ks, ka, kr;
+
+    BRDF() {}
+    BRDF(Color kd, Color ks, Color ka, Color kr) {
+      this->kd = kd;
+      this->ks = ks;
+      this->ka = ka;
+      this->kr = kr;
+    }
+};
 
 #endif
