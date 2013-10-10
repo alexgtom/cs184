25ed24448f9c798e04429acaa22c890f5da9155c
Alex Tom <alexgtom@berkeley.edu>
2013-10-10 01:09:28 -0700

    Made Point inherit from Vector

--- as2/base/Point.h
+++ as2/base/Point.h
@@ -2,47 +2,16 @@
 
 #include "Vector.h"
 
-class Point {
+class Point : public Vector {
 public:
-    float x, y, z;
-
     Point() {
-      x = y = z = 0;
     }
 
-	Point(float x, float y, float z) {
-      this->x = x;
-      this->y = y;
-      this->z = z;
+	Point(float x, float y, float z) : Vector(x, y, z) {
     }
-	Point(const Vector& v) { 
-      x = v.x;
-      y = v.y;
-      z = v.z;
+	Point(const Vector& v) : Vector(v) { 
     }
 
-	Point(const Point& p) {
-      x = p.x;
-      y = p.y;
-      z = p.z;
-    }
-
-	Point& operator=(const Point& p) {
-      x = p.x;
-      y = p.y;
-      z = p.z;
-	}
-	~Point() {}
-
-    bool operator==(const Point& p) const {
-      return x == p.x && y == p.y && z == p.z;
-    }
-
-    Point operator+(const Vector& w) {
-      return Point(x + w.x, y + w.y, z + w.z);
-    }
-
-    Point operator-(const Vector& w) {
-      return Point(x - w.x, y - w.y, z - w.z);
+	Point(const Point& p) : Vector(p) {
     }
 };
