4254441992cb149fdb183d4418e3d23957e58965
Alex Tom <alexgtom@berkeley.edu>
2013-10-10 03:08:48 -0700

    Remove GLUT stuff. Output is written to file.

--- as2/main.cpp
+++ as2/main.cpp
@@ -32,100 +32,14 @@
 
 using namespace std;
 
-//****************************************************
-// Some Classes
-//****************************************************
-
-class Viewport;
-
-class Viewport {
-  public:
-    int w, h; // width and height
-};
-
-//****************************************************
-// Global Variables
-//****************************************************
-Viewport	viewport;
-
-
-//****************************************************
-// Simple init function
-//****************************************************
-void initScene(){
-
-  // Nothing to do here for this simple example.
-
-}
-
-//****************************************************
-// reshape viewport if the window is resized
-//****************************************************
-void myReshape(int w, int h) {
-  viewport.w = w;
-  viewport.h = h;
-
-  glViewport (0,0,viewport.w,viewport.h);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluOrtho2D(0, viewport.w, 0, viewport.h);
-
-}
-
-//****************************************************
-// function that does the actual drawing of stuff
-//***************************************************
-void myDisplay() {
-
-  glClear(GL_COLOR_BUFFER_BIT);	  // clear the color buffer
-
-  glMatrixMode(GL_MODELVIEW);			// indicate we are specifying camera transformations
-  glLoadIdentity();				        // make sure transformation is "zero'd"
-
-  // Start drawing
-  Scene scene(viewport.w, viewport.h);
-  scene.render();
-
-  glFlush();
-  glutSwapBuffers();					    // swap buffers (we earlier set double buffer)
-}
-
-
-// Spacebar quit
-void spacebarQuit(unsigned char key, int x, int y) {
-  if(key == 32) {
-    exit(0);
-  }
-}
+extern char* filename;
 
 int main(int argc, char *argv[]) {
   // Parse Arguments
   parseArgs(argc, argv);
 
-  //This initializes glut
-  glutInit(&argc, argv);
-
-  //This tells glut to use a double-buffered window with red, green, and blue channels 
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-
-  // Initalize theviewport size
-  viewport.w = 400;
-  viewport.h = 400;
-
-  //The size and position of the window
-  glutInitWindowSize(viewport.w, viewport.h);
-  glutInitWindowPosition(0,0);
-  glutCreateWindow(argv[0]);
-
-  initScene();							// quick function to set up scene
-
-  glutDisplayFunc(myDisplay);				// function to run when its time to draw something
-  glutReshapeFunc(myReshape);				// function to run when the window gets resized
-
-  glutKeyboardFunc(spacebarQuit);
-
-  glutMainLoop();						// 999  //TODO: this is supposed to be infinityloop that will keep drawing and resizing
-  // and whatever else
+  Scene scene(filename);
+  scene.render();
 
   return 0;
 }
