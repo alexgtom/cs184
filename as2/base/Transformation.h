67cb9d7808adc6c2aebb3dc5c84cbe2c4cb384b7
Alex Tom <alexgtom@berkeley.edu>
2013-10-17 02:43:37 -0700

    Implemented Transformation

--- as2/base/Transformation.h
+++ as2/base/Transformation.h
@@ -24,23 +24,35 @@ class Transformation {
     }
 
     Point operator*(const Point& p) {
-      return p;
+      Vector4f v(p.x, p.y, p.z, 1.0f);
+      Vector4f result = m * v;
+      return Point(result(0), result(1), result(2)) / (float) p.w;
     }
 
-    Vector operator*(const Vector& v) {
-      return v;
+    Vector operator*(const Vector& tv) {
+      Vector4f v(tv.x, tv.y, tv.z, 0.0f);
+      Vector4f result = m * v;
+      return Vector(result(0), result(1), result(2));
     }
 
     Normal operator*(const Normal& n) {
-      return n;
+      Vector4f v(n.x, n.y, n.z, n.w);
+      Vector4f result = m.inverse().transpose() * v;
+      return Normal(result(0), result(1), result(2));
     }
 
     Ray operator*(const Ray& r) {
-      return r;
+      Ray ray(r);
+      ray.pos = (*this) * r.pos;
+      ray.dir = (*this) * r.dir;
+      return ray;
     }
 
     LocalGeo operator*(const LocalGeo& l) {
-      return l;
+      LocalGeo local_geo(l);
+      local_geo.pos = (*this) * l.pos;
+      local_geo.normal = (*this) * l.normal;
+      return local_geo;
     }
 };
 
--- as2/test/TransformationTest.h
+++ as2/test/TransformationTest.h
@@ -6,10 +6,58 @@
 class TransformationTest : public ::testing::Test {
 };
 
-TEST_F(TransformationTest, OverloadTest) {
-  Transformation t;
-  Vector v(1, 2, 3);
-  Point p(1, 2, 3);
-  t * v;
-  t * p;
+//
+// Creates a matrix that translates objects to different coordinates
+Matrix4f translate(float x, float y, float z) {
+  Matrix4f m = MatrixXf::Identity(4, 4);
+  m(0, 3) = x;
+  m(1, 3) = y;
+  m(2, 3) = z;
+  return m;
 }
+
+Matrix4f scale(float x, float y, float z) {
+  Matrix4f m = MatrixXf::Identity(4, 4);
+  m(0, 3) = 1;
+  m(1, 3) = 1;
+  m(2, 3) = 1;
+
+  m(0, 0) = z;
+  m(1, 1) = y;
+  m(2, 2) = z;
+  return m;
+}
+
+TEST_F(TransformationTest, Point) {
+  Transformation t(translate(2, 2, 2));
+  EXPECT_EQ(Point(3, 4, 5), t * Point(1, 2, 3));
+  EXPECT_EQ(Point(1, 2, 3), t * Point(0, 2, 4, 2));
+}
+
+TEST_F(TransformationTest, Vector) {
+  Transformation t(scale(2, 2, 2));
+  EXPECT_EQ(Vector(2, 4, 6), t * Vector(1, 2, 3));
+}
+
+TEST_F(TransformationTest, Normal) {
+  Transformation t(scale(2, 2, 2));
+  EXPECT_EQ(Normal(0.5, 1, 1.5), t * Normal(1, 2, 3));
+}
+
+TEST_F(TransformationTest, Ray) {
+  Transformation t(scale(2, 2, 2));
+  Ray r = t * Ray(Point(1, 2, 3), Vector(1, 2, 3), 0.0f, 1.0f);
+
+  EXPECT_EQ(Vector(2, 4, 6), r.dir);
+  EXPECT_EQ(Point(3, 5, 7), r.pos);
+}
+
+TEST_F(TransformationTest, LocalGeo) {
+  Transformation t(scale(2, 2, 2));
+  LocalGeo l = t * LocalGeo(Point(1, 2, 3), Normal(1, 2, 3));
+
+  EXPECT_EQ(Normal(0.5, 1, 1.5), l.normal);
+  EXPECT_EQ(Point(3, 5, 7), l.pos);
+}
+
+
