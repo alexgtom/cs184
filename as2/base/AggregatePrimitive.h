e6df9b429de6ad26622884d326a1b8635e8d5aae
Alex Tom <alexgtom@berkeley.edu>
2013-10-18 11:45:48 -0700

    Fixed AggregatePrimitive not returning the correct intersection results.

--- as2/base/AggregatePrimitive.h
+++ as2/base/AggregatePrimitive.h
@@ -10,13 +10,12 @@ class HelperPrimitive {
   public:
     float dist;
     float thit;
-    bool intersect_result;
+    bool has_intersection;
     Intersection in;
 
     HelperPrimitive(Ray ray, Primitive* primitive) {
-      intersect_result = primitive->intersect(ray, &thit, &in);
-      Point p = ray.pos - in.local.pos;
-      dist = Vector(p.x, p.y, p.z).mag();
+      has_intersection = primitive->intersect(ray, &thit, &in);
+      dist = (ray.pos - in.local.pos).mag();
     }
 
 	bool operator <(const HelperPrimitive& hp) const {
@@ -34,14 +33,20 @@ class AggregatePrimitive : public Primitive {
 
     // thit is the thit to the closest shape that the ray hits
     bool intersect(Ray& ray, float* thit, Intersection* in) {
+      // if theres no shapes, return false
       if(list.size() == 0)
         return false;
 
       vector<HelperPrimitive> hp_list;
 
       for(int i = 0; i < list.size(); i++) {
-        hp_list.push_back(HelperPrimitive(ray, list[i]));
+        if (list[i]->intersectP(ray))
+          hp_list.push_back(HelperPrimitive(ray, list[i]));
       }
+
+      // if theres no intersections, return false
+      if (hp_list.size() == 0)
+        return false;
       
       // sort list in ascending order
       sort(hp_list.begin(), hp_list.end());
@@ -50,7 +55,7 @@ class AggregatePrimitive : public Primitive {
       // 0 is index of the closest shape
       *thit = hp_list[0].thit;
       *in = hp_list[0].in;
-      return hp_list[0].intersect_result;
+      return hp_list[0].has_intersection;
     }
 
     bool intersectP(Ray& ray) {
@@ -63,6 +68,15 @@ class AggregatePrimitive : public Primitive {
       exit(1);
       // This should never get called, because in->primitive will
       // never be an aggregate primitive
+    }
+
+    friend std::ostream& operator<< (std::ostream& stream, const AggregatePrimitive& v) {
+      stream << "AggregatePrimitive[";
+      for(int i = 0; i < v.list.size(); i++) {
+        stream << "  [" << i << "]: " << v.list[i];
+      }
+      stream << "]";
+      return stream;
     }
 };
 
--- as2/test/AggregatePrimitiveTest.h
+++ as2/test/AggregatePrimitiveTest.h
@@ -48,3 +48,33 @@ TEST_F(AggregatePrimitiveTest, Basic) {
   EXPECT_EQ(Point(0, 0, 3), in.local.pos);
   EXPECT_EQ(1.0f, thit);
 }
+
+TEST_F(AggregatePrimitiveTest, NoIntersectionWithNoShapes) {
+  vector<Primitive*> list;
+  AggregatePrimitive ap(list);
+  
+  // create a ray and shoot it down the z coordinate
+  Ray ray(Point(0, 0, 4), Vector(0, 0, -4), 0, 4);
+
+  float thit;
+  Intersection in;
+  EXPECT_FALSE(ap.intersect(ray, &thit, &in));
+}
+
+TEST_F(AggregatePrimitiveTest, NoIntersectionWithShapes) {
+  GeometricPrimitive s1 = sphere_factory(0, 0, 1);
+  GeometricPrimitive s2 = sphere_factory(0, 0, 2);
+
+  vector<Primitive*> list;
+  list.push_back(&s2);
+  list.push_back(&s1);
+
+  AggregatePrimitive ap(list);
+  
+  // create a ray and shoot it down the y coordinate
+  Ray ray(Point(0, 0, 4), Vector(0, 4, 0), 0, 4);
+
+  float thit;
+  Intersection in;
+  EXPECT_FALSE(ap.intersect(ray, &thit, &in));
+}
