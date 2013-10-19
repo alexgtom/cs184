fa36afb38ee698c04b200ce054fc494aefce780b
Alex Tom <alexgtom@berkeley.edu>
2013-10-18 23:18:13 -0700

    Added render_scenes.sh

---
+++ as2/render_scenes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+time  ./main -f testscenes/scene1.test
+time  ./main -f testscenes/scene2.test
+time  ./main -f testscenes/scene3.test
+time  ./main -f testscenes/scene4-emission.test
+time  ./main -f testscenes/scene4-specular.test
+time  ./main -f testscenes/scene5.test
+time  ./main -f testscenes/scene6.test
