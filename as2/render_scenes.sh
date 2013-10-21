b3c427586aecf9d50c0f127a48fdbd6f9b8e13d4
Alex Tom <alexgtom@berkeley.edu>
2013-10-20 22:04:20 -0700

    made render_scenes.sh not render scene5. that takes forever

--- as2/render_scenes.sh
+++ as2/render_scenes.sh
@@ -5,5 +5,5 @@ time  ./main -f testscenes/scene2.test
 time  ./main -f testscenes/scene3.test
 time  ./main -f testscenes/scene4-emission.test
 time  ./main -f testscenes/scene4-specular.test
-time  ./main -f testscenes/scene5.test
+#time  ./main -f testscenes/scene5.test # this test has a million spheres.
 time  ./main -f testscenes/scene6.test
