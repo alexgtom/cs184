3efc11f9bc93b4ede2def25d513bb530f065c4b4
Kristen Curry <KristenCurry@Kristen-Currys-MacBook-Pro.local>
2013-10-26 18:18:03 -0700

    padding 2.5 gflop

--- part1.c
+++ part1.c
@@ -8,21 +8,82 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
     int kern_cent_X = (KERNX - 1)/2;
     // the y coordinate of the kernel's center
     int kern_cent_Y = (KERNY - 1)/2;
+
+    
+    //make a new in matrix with padding
+    int in_width = data_size_X + 2 * kern_cent_X;
+    int in_height = data_size_Y + 2 * kern_cent_Y;
+    int in_size = in_width * in_height;
+    int counter = 0;
+    float in_padding[in_size];
+    int last_col = in_width - 1;
+    //    for (int i = 0; i < (data_size_X * data_size_Y); i++) {
+    //	printf("%f ,", in[i]);
+    //}
+
+    memset( in_padding, 0.0, in_size*sizeof(float));
+    
+    // for (int n = 0; n < in_width+1; n++) {
+    //	in_padding[n] = 0.0;
+    //}
+
+    
+    
+    for (int n = in_width+1; n < (in_size - in_width); n++) {
+	if (n%in_width == last_col) {
+	    // in_padding[n] = 0.0;
+	    //in_padding[n++] = 0.0;
+	    n++;
+	} else {
+	    in_padding[n] = in[counter];
+	    counter++;
+	}
+    }
+    // for (int n = in_size-in_width; n < in_size; n++) {
+    //	in_padding[n] = 0.0;
+    //}
+
+    //for (int i = 0; i<in_size; i++) {
+    //	printf("%f ,", in_padding[i]);
+    //}
     
     // main convolution loop
-	for(int x = 0; x < data_size_X; x++){ // the x coordinate of the output location we're focusing on
-		for(int y = 0; y < data_size_Y; y++){ // the y coordinate of theoutput location we're focusing on
-			for(int i = -kern_cent_X; i <= kern_cent_X; i++){ // kernel unflipped x coordinate
-				for(int j = -kern_cent_Y; j <= kern_cent_Y; j++){ // kernel unflipped y coordinate
-					// only do the operation if not out of bounds
-					if(x+i>-1 && x+i<data_size_X && y+j>-1 && y+j<data_size_Y){
+
+    //blocking
+
+    //    int blocksize = 40;
+
+	
+	for(int y = 0; y < data_size_Y; y++){ // the x coordinate of the output location we're focusing on
+		for(int x = 0; x < data_size_X; x++){ // the y coordinate of theoutput location we're focusing on
+			for(int j = -kern_cent_Y; j <= kern_cent_Y; j++){ // kernel unflipped x coordinate
+				for(int i = -kern_cent_X; i <= kern_cent_X; i++){ // kernel unflipped y coordinate
+				    //ORIGINAL CODE
+				    // only do the operation if not out of bounds
+				    //if(x+i>-1 && x+i<data_size_X && y+j>-1 && y+j<data_size_Y){
 						//Note that the kernel is flipped
-						out[x+y*data_size_X] += 
-								kernel[(kern_cent_X-i)+(kern_cent_Y-j)*KERNX] * in[(x+i) + (y+j)*data_size_X];
-					}
+				    // 	out[x+y*data_size_X] += 
+				    //				kernel[(kern_cent_X-i)+(kern_cent_Y-j)*KERNX] * in[(x+i) + (y+j)*data_size_X];
+				    //	}
+				    //
+				    //OUT WITH PADDING / NO IF STATEMENT
+				      out[x + y*data_size_X] +=
+				   	kernel[(kern_cent_X - i) + (kern_cent_Y - j)*KERNX] *
+				    	in_padding[(x+i+1) + (y+j+1)*in_width];
+				    
 				}
+				//float kern[12] 
+				   
+
+
+				
 			}
 		}
 	}
+
+	//	for(int n = 0; n < (data_size_X * data_size_Y); n++) {
+	//  printf("%f ,", out[n]);
+	//}
+		
 	return 1;
 }
